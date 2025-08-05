6ce5b8a33f1d0663a9cde5ae1e86b841a703ad0c
Clayton Hinderline <Hinderline.clayton@gmail.com>
2025-08-05 14:23:08 -0400

    Added Patrol AI Controller
    Created Patrol AI controller, and moved the Seek function from TankPawn class into AI Controller class.

--- Assets/Scripts/AI Scripts/AIControllerCoward.cs
+++ Assets/Scripts/AI Scripts/AIControllerCoward.cs
@@ -17,7 +17,7 @@ public class AIControllerCoward : AIController
     }
 
     // Update is called once per frame
-    public override void MakeDescisions()
+    public override void Update()
     {
  
 
