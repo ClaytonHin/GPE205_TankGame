fb5e16bd47dd178caea74af788e6922f50ab6cf9
Clayton Hinderline <Hinderline.clayton@gmail.com>
2025-08-05 14:29:08 -0400

    Fixed issue with AIController Script

--- Assets/Scripts/AI Scripts/AIController.cs
+++ Assets/Scripts/AI Scripts/AIController.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class AIController : Controller
 {
     // Create an enum of different states our AI can use / transition into
-    public enum AIStates { GUARD, CHASE, CHASEANDSHOOT, FLEE, BACKAWAY, BACKAWAYANDSHOOT, STANDANDSHOOT }
+    public enum AIStates { GUARD, CHASE, CHASEANDSHOOT, FLEE, BACKAWAY, BACKAWAYANDSHOOT, STANDANDSHOOT, PATROL }
     // Create a AI state variable to know which state we are currently in
     public AIStates currentState;
     // Create a float variable to store the amount of time it has been since the last state change
     protected float lastStateChangeTime;
     // Create a object to store the target we are shooting, or fleeing from
     protected GameObject target;
+    // Create a list to store the patrol points
+    public List<Transform> waypoints;
+    // Create a variable to store the integer value which which waypoint we are at; We will start at position 0
+    private int currentWaypoint = 0;
+    // Create a variable to store a float value that will stop the AI from seeking if that value is met
+    public float seekCutoffDistance = 1;
+    // Create a variable to store the total amount of distance we want to look ahead with the raycast
+    public float lookAheadDistance = 5;
 
     public override void Awake()
     {
@@ -99,6 +109,48 @@ public class AIController : Controller
 
     }
 
+    // Create a function that will spawn a raycast to determine if the AI is steering into a wall
+    public bool CanMoveForward()
+    {
+        // Createa a ray variable that will be used for the raycast
+        Ray raycast = new Ray();
+        // Get the position of the AI pawn, and set it as our raycasts origin/position
+        raycast.origin = pawn.transform.position;
+        // Get the rotation of the AI pawn, and set it as our raycasts direction
+        raycast.direction = pawn.transform.forward;
+        // Create a variable to hold information about any objects that are hit by the raycast
+        RaycastHit hitData = new RaycastHit();
+        // Check to see if the raycast has hit an object
+        if (Physics.Raycast(raycast, out hitData, lookAheadDistance))
+        {
+            // Check to ensure that the target hit was not the player
+            if (hitData.collider.gameObject != target)
+            {
+                // If he hit an object then return false
+                return false;
+            }
+        }
+
+        // If the raycast has not hit any objects then return true
+        return true;
+    }
+
+    // Create a helper function to handle the decicison making when an object is hit by the raycast
+    public void MoveWithAdvoidance()
+    {
+        // Check if we can move forward
+        if (CanMoveForward())
+        {
+            // Move the pawn/AI object forward
+            pawn.MoveForward();
+        }
+        else
+        {
+            // If CanMoveForward is false then rotate to avoid the obstacle
+            pawn.RotateClockwise();
+        }
+    }
+
     public void Guard()
     {
         // Rotate the AI Pawn
@@ -146,6 +198,48 @@ public class AIController : Controller
 
         // Move Backwards
         pawn.MoveBackward();
+    }
+
+    public void Patrol()
+    {
+        // Move to the current point that the AI is patrolling towards
+        Seek(waypoints[currentWaypoint].position);
+
+        // If the AI is within the seekCutoffDistance to that patrol point, then change patrol points to the next value/step/stage
+        if (Vector3.Distance(pawn.transform.position, waypoints[currentWaypoint].position) <= seekCutoffDistance)
+        {
+            // Increment our current waypoint
+            currentWaypoint++;
+            // Check to ensure that our current waypoint value doesn't exceed the maximum number of waypoints.
+            if (currentWaypoint >= waypoints.Count)
+            {
+                // If our currentWaypoint exceeds the total amount of waypoints, then loop back to zero
+                currentWaypoint = 0;
+            }
+        }
+    }
+
+    // Create a function to seek out a specific position
+    public void Seek(Vector3 positionToSeek)
+    {
+        // Rotate twoards the target position
+        pawn.RotateTowards(positionToSeek);
+
+        // Move the Pawn forward
+        pawn.MoveForward();
+    }
+
+    // Create a function for the seek function, to search for a gameobject's position
+    public void Seek(GameObject objectToSeek)
+    {
+        // Call the seek method and pass in the objects position/transform
+        Seek(objectToSeek.transform.position);
+    }
+
+    // Create a function for the seek function, to search for a controller's pawn position
+    public void Seek(Controller controllerToSeek)
+    {
+        Seek(controllerToSeek.pawn.gameObject);
     }
 
     public override void MakeDescisions()
